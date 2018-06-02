1c0409c84cf2c713d69826ada263a304abbec462
root <root@localhost.localdomain>
2018-06-02 18:04:53 -0500

    Working on it

--- week-02/index.js
+++ week-02/index.js
@@ -9,12 +9,23 @@ const options = {
   // TODO: Create an object literal to hit your account on the GitHub API,
   // and pass any other information the API requires (you should not have
   // to authenticate for this, however)
+
+  uri: 'https://api.github.com/users/csiegfri',
+  headers: {
+    'User-Agent': 'Request-Promise'
+  },
+  json: true;
+
 };
 
 req(options)
   .then(
     // TODO: Handle the returned JSON data and write it to a file called
     // `response.json` in your `week-two/` directory
+    var obj = {
+
+    }
+    console.log(options)
   )
   .catch(function(err) {
     console.log(err);
