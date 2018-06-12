6f33928c2b0332e331471d779fc296b1a3301356
root <root@localhost.localdomain>
2018-06-12 09:24:18 -0500

    Adjusted Index to grab JSON

--- week-03/github/routes/index.js
+++ week-03/github/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+// Re-grabbing the code from Week 2 and Week 1
+const requ = require('request-promise-native');
+
+const options = {
+  uri: 'https://api.github.com/users/csiegfri',
+  headers: {
+    'User-Agent': 'Request-Promise'
+  },
+  json: true
+
+};
+
+requ(options)
+  .then(function(profile_json){
+    var jlogin = JSON.stringify(profile_json.login);
+    var jid = JSON.stringify(profile_json.id);
+    var javatar = JSON.stringify(profile_json.avatar_url);
+    var jhtml_url = JSON.stringify(profile_json.html_url);
+    var jname = JSON.stringify(profile_json.name);
+    var jbio = JSON.stringify(profile_json.bio);
+    var jupdated_at = JSON.stringify(profile_json.updated_at);
+);
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Index'
+
+
+
+
+  });
 });
 
 module.exports = router;
