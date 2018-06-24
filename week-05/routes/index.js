6be525ccaf45e0d45e4a73a145c6d30518583171
root <root@localhost.localdomain>
2018-06-24 10:47:08 -0500

    added request packages

--- week-05/routes/index.js
+++ week-05/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+let fall = null;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fetch('/public/api/fall.json')
+    .then(response => response.json())
+    .then(responseJSON => fall);
+  let example = fall.metadata.semester;
+  res.render('index', { title: example });
 });
 
 module.exports = router;
